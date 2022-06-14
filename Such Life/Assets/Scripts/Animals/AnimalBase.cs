c733d8b23ab16e8af7e214ca777b31082a472708
QuackGameDev <107073102+QuackGameDev@users.noreply.github.com>
2022-06-14 14:54:39 -0400

    AnimalBase Update
    Removed a package import that doesn't exist

--- Such Life/Assets/Scripts/Animals/AnimalBase.cs
+++ Such Life/Assets/Scripts/Animals/AnimalBase.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-using UnityEngine.AnimationModule;
 
 /**
  * This is the base for all animal AI
