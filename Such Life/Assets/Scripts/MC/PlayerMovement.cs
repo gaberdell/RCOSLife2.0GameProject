d02f7ae1340fbccafb33f1dc95f3d2232fecf05a
Quang Gia Nguyen <91767053+LordChunky@users.noreply.github.com>
2022-06-15 17:06:51 -0400

    Complete Player movement
    Finish writing code for the player movement with animation of the place holder sprite for MC

--- Such Life/Assets/Scripts/MC/PlayerMovement.cs
+++ Such Life/Assets/Scripts/MC/PlayerMovement.cs
@@ -5,42 +5,59 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D body;
-    //public SpriteRenderer spriteRenderer;
-
-
-
+    public Animator anim;
+    public Transform interactor;
     public float walkSpeed;
-    public float frameRate;
-
     Vector2 direction;
-
-    /*
-    void SpriteFlip() {
-        //if we are facing right, and the player holds left, flip.
-        if(!spriteRenderer.flipX && direction.x < 0){
-            spriteRenderer.flipX = true;
-        } else if (spriteRenderer.flipX && direction.x > 0) /*if we are facing left, and the player holds right, flip.
-        {
-            spriteRenderer.flipX = false;
-        }
-    }
-    */
-
-    // Start is called before the first frame update
-    void Start() {
-        
-    }
 
     // Update is called once per frame
     void Update() {
         //character movement
         direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
-        
+        anim.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+        anim.SetFloat("Vertical", Input.GetAxis("Vertical"));
+        anim.SetFloat("Speed", direction.sqrMagnitude);
+
+
+
+        //remember the last horizontal and vertical float to set the idle animation correctly
+        if(Input.GetAxis("Horizontal") == 1 || Input.GetAxis("Horizontal") == -1 || Input.GetAxis("Vertical") == 1 || Input.GetAxis("Vertical") == -1) {
+            anim.SetFloat("LastHorizontal", Input.GetAxis("Horizontal"));
+            anim.SetFloat("LastVertical", Input.GetAxis("Vertical"));
+        }
+
+
+        //give the game info of the direction that the player is facing (for interaction feature later)
+        if(Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") > 0) /*N*/ {
+            interactor.localRotation = Quaternion.Euler(0, 0, 180);
+        }
+        if(Input.GetAxis("Horizontal") > 0 && Input.GetAxis("Vertical") > 0) /*NE*/ {
+            interactor.localRotation = Quaternion.Euler(0, 0, 135);
+        } 
+        if(Input.GetAxis("Horizontal") > 0 && Input.GetAxis("Vertical") == 0) /*E*/ {
+            interactor.localRotation = Quaternion.Euler(0, 0, 90);
+        }   
+        if(Input.GetAxis("Horizontal") > 0 && Input.GetAxis("Vertical") < 0) /*SE*/ {
+            interactor.localRotation = Quaternion.Euler(0, 0, 45);
+        }
+        if(Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") < 0) /*S*/ {
+            interactor.localRotation = Quaternion.Euler(0, 0, 0);
+        }
+        if(Input.GetAxis("Horizontal") < 0 && Input.GetAxis("Vertical") < 0) /*SW*/ {
+            interactor.localRotation = Quaternion.Euler(0, 0, -45);
+        }
+        if(Input.GetAxis("Horizontal") < 0 && Input.GetAxis("Vertical") == 0) /*W*/ {
+            interactor.localRotation = Quaternion.Euler(0, 0, -90);
+        }
+        if(Input.GetAxis("Horizontal") < 0 && Input.GetAxis("Vertical") > 0) /*NW*/ {
+            interactor.localRotation = Quaternion.Euler(0, 0, -135);
+        }
+
+
     }
 
     void FixedUpdate() {
         //Movement
-        //SpriteFlip();
         body.MovePosition(body.position + direction * walkSpeed * Time.fixedDeltaTime);
     }
 }
