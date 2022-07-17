ef862d66183f9e2da82e1b6eec58ba9d167bf818
Quang Gia Nguyen <91767053+LordChunky@users.noreply.github.com>
2022-07-16 20:09:21 -0400

    (WiP) Inventory System and Hotbar
    I have layout barebone structure for an inventory system and create a basic hotbar for the MC

--- Such Life/Assets/Scripts/MC/PlayerMovement.cs
+++ Such Life/Assets/Scripts/MC/PlayerMovement.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -10,54 +12,69 @@ public class PlayerMovement : MonoBehaviour
     public float walkSpeed;
     Vector2 direction;
 
+
+    private float inputX;
+    private float inputY;
+
     // Update is called once per frame
     void Update() {
         //character movement
-        direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
-        anim.SetFloat("Horizontal", Input.GetAxisRaw("Horizontal"));
-        anim.SetFloat("Vertical", Input.GetAxisRaw("Vertical"));
+        direction = new Vector2(inputX, inputY).normalized;
+        anim.SetFloat("Horizontal", inputX);
+        anim.SetFloat("Vertical", inputY);
         anim.SetFloat("Speed", direction.sqrMagnitude);
 
 
 
         //remember the last horizontal and vertical float to set the idle animation correctly
-        if(Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 || Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1) {
-            anim.SetFloat("LastHorizontal", Input.GetAxisRaw("Horizontal"));
-            anim.SetFloat("LastVertical", Input.GetAxisRaw("Vertical"));
+        if (inputX == 1 || inputX == -1 || inputY == 1 || inputY == -1) {
+            anim.SetFloat("LastHorizontal", inputX);
+            anim.SetFloat("LastVertical", inputY);
         }
 
 
         //give the game info of the direction that the player is facing (for interaction feature later)
-        if(Input.GetAxisRaw("Horizontal") == 0 && Input.GetAxisRaw("Vertical") > 0) /*N*/ {
+        if (inputX == 0 && inputY > 0) /*N*/ {
             interactor.localRotation = Quaternion.Euler(0, 0, 180);
         }
-        if(Input.GetAxisRaw("Horizontal") > 0 && Input.GetAxisRaw("Vertical") > 0) /*NE*/ {
+        if (inputX > 0 && inputY > 0) /*NE*/ {
             interactor.localRotation = Quaternion.Euler(0, 0, 135);
-        } 
-        if(Input.GetAxisRaw("Horizontal") > 0 && Input.GetAxisRaw("Vertical") == 0) /*E*/ {
+        }
+        if (inputX > 0 && inputY == 0) /*E*/ {
             interactor.localRotation = Quaternion.Euler(0, 0, 90);
-        }   
-        if(Input.GetAxisRaw("Horizontal") > 0 && Input.GetAxisRaw("Vertical") < 0) /*SE*/ {
+        }
+        if (inputX > 0 && inputY < 0) /*SE*/ {
             interactor.localRotation = Quaternion.Euler(0, 0, 45);
         }
-        if(Input.GetAxisRaw("Horizontal") == 0 && Input.GetAxisRaw("Vertical") < 0) /*S*/ {
+        if (inputX == 0 && inputY < 0) /*S*/ {
             interactor.localRotation = Quaternion.Euler(0, 0, 0);
         }
-        if(Input.GetAxisRaw("Horizontal") < 0 && Input.GetAxisRaw("Vertical") < 0) /*SW*/ {
+        if (inputX < 0 && inputY < 0) /*SW*/ {
             interactor.localRotation = Quaternion.Euler(0, 0, -45);
         }
-        if(Input.GetAxisRaw("Horizontal") < 0 && Input.GetAxisRaw("Vertical") == 0) /*W*/ {
+        if (inputX < 0 && inputY == 0) /*W*/ {
             interactor.localRotation = Quaternion.Euler(0, 0, -90);
         }
-        if(Input.GetAxisRaw("Horizontal") < 0 && Input.GetAxisRaw("Vertical") > 0) /*NW*/ {
+        if (inputX < 0 && inputY > 0) /*NW*/ {
             interactor.localRotation = Quaternion.Euler(0, 0, -135);
         }
-
-
     }
+
+    //get whatever input it being send in by the InputSystem
+    public void Move(InputAction.CallbackContext context)
+    {
+        inputX = context.ReadValue<Vector2>().x;
+        inputY = context.ReadValue<Vector2>().y;
+    }
+
 
     void FixedUpdate() {
         //Movement
-        body.MovePosition(body.position + direction * walkSpeed * Time.fixedDeltaTime);
+        //body.MovePosition(body.position + direction * walkSpeed * Time.fixedDeltaTime);
+
+        body.velocity = new Vector2(inputX * walkSpeed, inputY * walkSpeed);
+
+
+
     }
 }
