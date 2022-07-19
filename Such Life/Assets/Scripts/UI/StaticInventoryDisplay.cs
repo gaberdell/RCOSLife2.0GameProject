f252495bd03ccaec9cfa77c5588c35952fd0ff02
Quang Gia Nguyen <91767053+LordChunky@users.noreply.github.com>
2022-07-19 16:10:38 -0400

    Update Inventory System
    Update Hotbar inventory for the MC

--- Such Life/Assets/Scripts/UI/StaticInventoryDisplay.cs
+++ Such Life/Assets/Scripts/UI/StaticInventoryDisplay.cs
@@ -19,11 +19,28 @@ public class StaticInventoryDisplay : InventoryDisplay
             inventorySystem.OnInventorySlotChanged += UpdateSlot;
 
         }
+        else
+        {
+            Debug.LogWarning($"No inventory assigned to {this.gameObject}");
+        }
+
+        AssignSlot(inventorySystem);
     }
 
     public override void AssignSlot(InventorySystem invToDisplay)
     {
-        throw new System.NotImplementedException();
+        slotDictionary = new Dictionary<InventorySlot_UI, InventorySlot>();
+
+        if(slots.Length != inventorySystem.InventorySize)
+        {
+            Debug.Log($"Inventory slot out of sync on this {this.gameObject}");
+        }
+
+        for (int i = 0; i < inventorySystem.InventorySize; i++)
+        {
+            slotDictionary.Add(slots[i], inventorySystem.InventorySlots[i]);
+            slots[i].Init(inventorySystem.InventorySlots[i]);  
+        }
     }
 
     
