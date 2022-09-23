73a14a385ffc7c184205a10459cd05f1d6c6001d
alittlerocket <84694604+alittlerocket@users.noreply.github.com>
2022-09-23 18:32:27 -0400

    Slimmer update
    It follows you! But it has some weird jiggle mechanic in attempt to remove acceleration and deceleration.

--- Such Life/Assets/Scripts/hostile mob/Slim/Slimmer.cs
+++ Such Life/Assets/Scripts/hostile mob/Slim/Slimmer.cs
@@ -8,13 +8,17 @@ public class Slimmer : mobBase
     private float timeDelay;
     private float time;
     private float alertRange;
+    public float distance;
     // Start is called before the first frame update
+    public Transform target;
+    public NavMeshAgent agent;
     public SpriteRenderer slimeSprite;
     
     void Start()
     {
-        target = GameObject.FindGameObjectWithTag("Player").transform;
+        target = GameObject.Find("MC").transform;
         agent = GetComponent<NavMeshAgent>();
+        agent.acceleration = 100;
         agent.updateRotation = false;
         agent.updateUpAxis = false;
 
@@ -26,16 +30,18 @@ public class Slimmer : mobBase
         time_move = 3.0f;
         currPosition = new Vector2(transform.position.x, transform.position.y);
 
-        timeDelay = 400f;
+        timeDelay = 4f;
         time = 0f;
+        distance = 0f;
         alertRange = 6.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        time += 1f;
-        float distance = findDistance(currPosition.x, target.position.x, currPosition.y, target.position.x);
+        target = GameObject.Find("MC").transform;
+        time += 1f * Time.deltaTime;
+        distance = Vector2.Distance(target.position, currPosition);
         if (distance <= alertRange && !playerSighted)
         {
             playerSighted = true;
@@ -43,7 +49,7 @@ public class Slimmer : mobBase
         }
         if (time >= timeDelay)
         {
-            time = 0;
+            time = 0f;
             if (playerSighted)
             {
                 chasing(distance);
@@ -66,18 +72,14 @@ public class Slimmer : mobBase
     void chasing(float distance)
     {
         float oldPosX = currPosition.x;
-        float angle = Mathf.Atan((target.position.y - currPosition.y) / (target.position.x - currPosition.x));
-        if (distance >= 4.0f)
-        {
-            this.currPosition = new Vector2(currPosition.x + 4 * Mathf.Cos(angle), currPosition.y + 4 * Mathf.Sin(angle));
-        }
-        else if (distance < 1.0f)
+        float angle = Mathf.Atan2((target.position.y - currPosition.y) , (target.position.x - currPosition.x));
+        if (distance >= 1.8f)
         {
             this.currPosition = new Vector2(currPosition.x + 2 * Mathf.Cos(angle), currPosition.y + 2 * Mathf.Sin(angle));
         }
         else
         {
-            this.currPosition = new Vector2(currPosition.x + 3 * Mathf.Cos(angle), currPosition.y + 3 * Mathf.Sin(angle));
+            this.currPosition = new Vector2(currPosition.x + 1 * Mathf.Cos(angle), currPosition.y + 1 * Mathf.Sin(angle));
         }
         flipSprite(oldPosX);
         agent.SetDestination(currPosition);
@@ -103,11 +105,6 @@ public class Slimmer : mobBase
         {
             slimeSprite.flipX = false;
         }
-    }
-
-    float findDistance(float x1, float y1, float x2, float y2)
-    {
-        return Mathf.Sqrt(Mathf.Pow(x1 - x2, 2) + Mathf.Pow(y1 - y2, 2));
     }
 
 }
