ff527acad482d876f17972a8348fe2c03916951e
Alex Mattoni <alex.mattoni@gmail.com>
2022-11-01 17:36:04 -0400

    Audio Testing Scene + Update Music_Timer Script
    Created an Audio Testing Unity Scene to keep development of audio scripts separate from everything else. Updated the music_timer.cs C# script to create AudioSource & AudioListener objects for the thing it is attached to for easy "drag & drop" implementation.

--- Such Life/Assets/Scripts/Sound/music_timer.cs
+++ Such Life/Assets/Scripts/Sound/music_timer.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class music_timer : MonoBehaviour
 {
-    public float timer = 21.295f;
+    public float timer = 0;
+    public AudioClip battleTheme;
+    public AudioSource source1;
+    public AudioListener listener1;
+
+    void Start()
+    {
+        source1 = gameObject.AddComponent<AudioSource>();
+        listener1 = gameObject.AddComponent<AudioListener>();
+    }
 
     // Called every frame (decrement timer by the elapsed
     // time between frames, every frame)
     void Update()
     {
-        timer -= Time.deltaTime;
+        if(timer > 0)
+            timer -= Time.deltaTime;
     }
     // When colliding with something, if the timer has run out,
     // play the given sound
@@ -18,8 +28,8 @@ public class music_timer : MonoBehaviour
     {
         if (timer <= 0)
         {
-            SendMessage("Play");
             timer = 21.295f;
+            source1.PlayOneShot(battleTheme);
         }
     }
 
