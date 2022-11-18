e99b303ebb371b0bbf32146befb8fc22f105b489
QuackGameDev <107073102+QuackGameDev@users.noreply.github.com>
2022-11-18 17:20:58 -0500

    Easier Spawners
    Made a spawner base so that spawners can be created more efficiently. Also added types so that spawners can't spawn in places they can't be. Grass sprite has been changed due to requests...

--- Such Life/Assets/Scripts/Spawners/Grass.cs
+++ Such Life/Assets/Scripts/Spawners/Grass.cs
@@ -2,18 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Grass : MonoBehaviour
+public class Grass : SpawnerBase
 {
-    public GameObject GrassObj;
-    private Vector2 spawnerPos;
-    public float patchsize;
-    public float timeDelay;
-    private float time;
-
     // Start is called before the first frame update
     void Start()
     {
-        GrassObj = Resources.Load("Prefab/Grass") as GameObject;
+        SpawnObj = Resources.Load("Prefab/Grass") as GameObject;
         time = 29f;
         timeDelay = 3f;
         float genx = Random.Range(-5f, 5f);
@@ -21,19 +15,10 @@ public class Grass : MonoBehaviour
         Vector2 temp = new Vector2(genx, geny);
         this.transform.position = temp;
         spawnerPos = this.transform.position;
-        patchsize = Random.Range(0f, 5f);
+        spawnsize = Random.Range(0f, 5f);
+        type.Add("land");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        time = time + 1f * Time.deltaTime;
-        if (time >= timeDelay)
-        {
-            float x = Random.Range(-patchsize, patchsize);
-            float y = Random.Range(-patchsize, patchsize);
-            var growGrass = Instantiate(GrassObj, new Vector3(x + spawnerPos.x, y + spawnerPos.y, 0), Quaternion.identity);
-            time = 0;
-        }
-    }
+
+
 }
--- Such Life/Assets/Scripts/Spawners/Sheepocalypse.cs
+++ Such Life/Assets/Scripts/Spawners/Sheepocalypse.cs
@@ -2,17 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Sheepocalypse : MonoBehaviour
+public class Sheepocalypse : SpawnerBase
 {
-    public GameObject SheepObj;
-    private Vector2 spawnerPos;
-    private float gensize;
-    public float timeDelay;
-    private float time;
     // Start is called before the first frame update
     void Start()
     {
-        SheepObj = Resources.Load("Prefab/ScuffedSheep") as GameObject;
+        SpawnObj = Resources.Load("Prefab/ScuffedSheep") as GameObject;
         time = 29f;
         timeDelay = 10f;
         float genx = Random.Range(-10f, 10f);
@@ -20,19 +15,9 @@ public class Sheepocalypse : MonoBehaviour
         Vector2 temp = new Vector2(genx, geny);
         this.transform.position = temp;
         spawnerPos = this.transform.position;
-        gensize = Random.Range(0f, 5f);
+        spawnsize = Random.Range(0f, 5f);
+        type.Add("land");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        time = time + 1f * Time.deltaTime;
-        if (time >= timeDelay)
-        {
-            float x = Random.Range(-gensize, gensize);
-            float y = Random.Range(-gensize, gensize);
-            var growGrass = Instantiate(SheepObj, new Vector3(x + spawnerPos.x, y + spawnerPos.y, 0), Quaternion.identity);
-            time = 0;
-        }
-    }
+
 }
