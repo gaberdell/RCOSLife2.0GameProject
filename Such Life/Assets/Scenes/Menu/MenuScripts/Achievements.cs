77c67ea9312853291aef9add5cd6cd3bc5ae7982
Alex Mattoni <alex.mattoni@gmail.com>
2022-11-29 17:22:31 -0500

    menu development last commit
    Minor changes to the Achievements script & built a testing .exe for demonstration purposes.

--- Such Life/Assets/Scenes/Menu/MenuScripts/Achievements.cs
+++ Such Life/Assets/Scenes/Menu/MenuScripts/Achievements.cs
@@ -4,28 +4,35 @@ using UnityEngine;
 
 public class Achievements : MonoBehaviour
 {
+    public Dictionary<string, Achievement> tracker;
+
+    public class Achievement
+    {
+        public string name;
+        public string description;
+        public bool has;
+    }
+
+    public void setAchievement(string request)
+    {
+        if (tracker.ContainsKey(request))
+        {
+            tracker[request].has = true;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         // initializes the Dictionary which tracks whether the player has an achievement
         // a separate Dictionary can be made to track the associated description,
         // or this one can be expanded.
-        private Dictionary<string, bool> tracker = new Dictionary<string, bool>
+        tracker = new Dictionary<string, Achievement>
         {
             // insert achievements here, initialize to false
-            {"Play the game!", true},
-            {"Vanquish a Slimmer!", false},
-            {"Test Achievement", false}
+            {"Play the game!", new Achievement {name="Play the game!", description="TBD", has=true }  },
+            {"Vanquish a Slimmer!", new Achievement {name="Vaniquish a Slimmer!", description="TBD", has=false }  },
+            {"Test Achievement", new Achievement{name="Test Achievement", description="TBD", has=false } }
         };
     }
-
-    public void setAchievement(string request)
-    {
-        if(tracker.ContainsKey(request))
-        {
-            tracker[request] = true;
-        }
-    }
-    
-    
 }
