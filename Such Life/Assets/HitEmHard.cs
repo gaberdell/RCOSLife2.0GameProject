01c011963806f452b1b3014f274ae602322f9836
Erica Nazimowitz <nazime@rpi.edu>
2023-03-14 16:33:40 -0400

    Update HitEmHard.cs

--- Such Life/Assets/HitEmHard.cs
+++ Such Life/Assets/HitEmHard.cs
@@ -4,21 +4,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-
-
-
 public class HitEmHard : MonoBehaviour
 {
-    [SerializeField]
-    private InputActionReference hit;
-
+    public playerAction playerControls;
+    private InputAction attack;
 
     private weaponParent wparent;
+
+    public bool animation_go;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerControls = new playerAction();
     }
 
     // Update is called once per frame
@@ -27,11 +25,21 @@ public class HitEmHard : MonoBehaviour
         
     }
 
+    private void PerformAttack(InputAction.CallbackContext obj) {
+        wparent.Attack();
+    }
+
+
+
     private void onEnable() {
-        attack.action.performed += PerformAttack;
+        playerControls.Player.Enable();
+        attack = playerControls.Player.Attack;
+        attack.Enable();
+        attack.performed += PerformAttack;
     }
 
     private void onDisable() {
-        attack.action.performed -= PerformAttack;
+        playerControls.Player.Disable();
+        attack.Disable();
     }
 }
