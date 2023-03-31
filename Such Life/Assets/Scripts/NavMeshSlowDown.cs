3cfc052c355467b1aeab580bc7843e22d0a12a61
Michael Lyga <31606955+mkwiiallpro@users.noreply.github.com>
2023-03-31 15:43:55 -0400

    New navmesh agent types
    "humanoid" is slowed by climbing, swimming, everything really
    "can fly" is unaffected by all navmesh modifiers
    "can swim" can swim at 1x speed but is otherwise the same as humanoid

--- Such Life/Assets/Scripts/NavMeshSlowDown.cs
+++ Such Life/Assets/Scripts/NavMeshSlowDown.cs
@@ -10,30 +10,43 @@ public class NavMeshSlowDown : MonoBehaviour{
         mod = GetComponent<NavMeshModifier>();
     }
 
-    private void OnCollisionEnter2D(Collision2D other){
+    /*private void OnCollisionEnter2D(Collision2D other){
         float CostModifier = NavMesh.GetAreaCost(mod.area);
         Rigidbody2D agent = other.gameObject.GetComponent<Rigidbody2D>();
         agent.velocity /= CostModifier;
        
-    }
+    }*/
     private void OnTriggerEnter2D(Collider2D col){
-        float CostModifier = NavMesh.GetAreaCost(mod.area);
+        float CostModifier;
+        switch(mod.area){
+            case 3: CostModifier = 2.0F; break;
+            case 4: CostModifier = 4.0F; break;
+            default: CostModifier = 1.0F; break;
+        }
         Debug.Log("Enter");
-        if(col.gameObject.tag == "Player"){
+        if(col.gameObject.tag == "Player"|| col.gameObject.tag == "Untagged"){
+            
             col.gameObject.GetComponent<PlayerMovement>().walkSpeed /= CostModifier;
+            
+           
         }
     }
     private void OnTriggerExit2D(Collider2D col){
-        float CostModifier = NavMesh.GetAreaCost(mod.area);
+        float CostModifier;
+        switch(mod.area){
+            case 3: CostModifier = 2.0F; break;
+            case 4: CostModifier = 4.0F; break;
+            default: CostModifier = 1.0F; break;
+        }
         Debug.Log("Exit");
-        if(col.gameObject.tag == "Player"){
+        if(col.gameObject.tag == "Player" || col.gameObject.tag == "Untagged"){
             col.gameObject.GetComponent<PlayerMovement>().walkSpeed *= CostModifier;
         }
     }
-    private void OnCollisionExit2D(Collision2D other){
+    /*private void OnCollisionExit2D(Collision2D other){
         float CostModifier = NavMesh.GetAreaCost(mod.area);
         Rigidbody2D agent = other.gameObject.GetComponent<Rigidbody2D>();
         agent.velocity *= CostModifier;
         
-    }
+    }*/
 }
