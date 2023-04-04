9d768cb2893198a220e312502cc77f2d922b8867
Rorymar <36866664+Rorymar@users.noreply.github.com>
2023-04-04 17:32:25 -0400

    Created basic alignment system for infobox
    The infobox now aligns to one of four quadrants. The infobox does not yet handle quadrant overlap and encounters issues with item squares covering it.

--- Such Life/Assets/MouseOver.cs
+++ Such Life/Assets/MouseOver.cs
@@ -6,10 +6,10 @@ using UnityEngine.UI;
 
 public class MouseOver : MonoBehaviour
 {
-
+    public int quad = 0;  
+    Vector3 pos;
     public void Awake(){
-        Debug.Log("You Mean I Wasted My Tomato Sauce?");
-        
+        pos = new Vector3(transform.position.x,transform.position.y,transform.position.z);
     }
 
     public void TooltipOn(){
@@ -22,22 +22,32 @@ public class MouseOver : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.name == "Quad1")
+        if (collision.gameObject.name == "Quad1" && quad == 0)
         {
-            Debug.Log("Do something here 1");
+            quad = 1;
+            transform.position = new Vector3(pos.x-7.7f,pos.y-2.9f,pos.z);
         }
-        if (collision.gameObject.name == "Quad2")
+        else if (collision.gameObject.name == "Quad2" && quad == 0)
         {
-            Debug.Log("Do something here 2");
+            quad = 2;
+            transform.position = new Vector3(pos.x-7.7f,pos.y+18.0f,pos.z);
         }
-        if (collision.gameObject.name == "Quad3")
+        else if (collision.gameObject.name == "Quad3" && quad == 0)
         {
-            Debug.Log("Do something here 3");
+            quad = 3;
+            transform.position = new Vector3(pos.x+17.8f,pos.y+18.0f,pos.z);
         }
-        if (collision.gameObject.name == "Quad4")
+        else if (collision.gameObject.name == "Quad4" && quad == 0)
         {
-            Debug.Log("Do something here 4");
+            quad = 4;
+            transform.position = new Vector3(pos.x+17.8f,pos.y-2.9f,pos.z);
         }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        transform.position = pos;
+        quad = 0;
     }
 
 }
