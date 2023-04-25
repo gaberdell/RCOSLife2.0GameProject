5d9723eff5765fa17d0ec1c1cc79bf7209a4a9c9
Jae7777 <98072012+Jae7777@users.noreply.github.com>
2023-04-24 22:14:16 -0400

    Expanded Key Binds and Removed Previous Feature
    -Add more key bindings (inventory, hotbars, menu toggle)
    -Added a scroll view to the controls panel.
    -Removed the feature that highlights overlapping key binds as red (the implementation was buggy and unresourceful, am looking for a better way to implement this feature).

--- Such Life/Assets/Scenes/Menu/MenuScripts/Settings/KeyBindScript.cs
+++ Such Life/Assets/Scenes/Menu/MenuScripts/Settings/KeyBindScript.cs
@@ -10,15 +10,26 @@ public class KeyBindScript : MonoBehaviour
     // syntax of key must match exactly what each text in the texts array below is
     // order does not necessarily matter
     private Dictionary<string, KeyCode> defaultKeyBinds = new Dictionary<string, KeyCode> {
+        {"Toggle Menu", KeyCode.Escape},
         {"Move Up", KeyCode.W},
         {"Move Left", KeyCode.A},
         {"Move Down", KeyCode.S},
         {"Move Right", KeyCode.D},
         {"Primary", KeyCode.Mouse0},
-        {"Skill 1", KeyCode.R}
+        {"Skill 1", KeyCode.R},
+        {"Skill 2", KeyCode.T},
+        {"Hotbar 1", KeyCode.Alpha1},
+        {"Hotbar 2", KeyCode.Alpha2},
+        {"Hotbar 3", KeyCode.Alpha3},
+        {"Hotbar 4", KeyCode.Alpha4},
+        {"Hotbar 5", KeyCode.Alpha5},
+        {"Hotbar 6", KeyCode.Alpha6},
+        {"Hotbar 7", KeyCode.Alpha7},
+        {"Hotbar 8", KeyCode.Alpha8},
+        {"Hotbar 9", KeyCode.Alpha9},
+        {"Hotbar 10", KeyCode.Alpha0},
+        {"Toggle Inventory", KeyCode.Tab}
     };
-
-    private Dictionary<string, KeyCode> currentKeyBinds = new Dictionary<string, KeyCode> {};
 
     public TextMeshProUGUI[] texts;
     public Button[] buttons;
@@ -41,9 +52,6 @@ public class KeyBindScript : MonoBehaviour
 
                 // Update the text of the Text component
                 buttonText.text = PlayerPrefs.GetString(text.text, defaultKeyBinds[text.text].ToString());
-
-                // intialize the dictionary of the player's current key bindings
-                currentKeyBinds[text.text] = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString(text.text, defaultKeyBinds[text.text].ToString()));
             }
         }
         else
@@ -57,8 +65,7 @@ public class KeyBindScript : MonoBehaviour
     void Update()
     {
         if (waitingForInput)
-        {
-            
+        {  
             if (Input.anyKeyDown)
             {   
                 foreach (KeyCode keyCode in Enum.GetValues(typeof(KeyCode)))
@@ -98,29 +105,6 @@ public class KeyBindScript : MonoBehaviour
                     finishInput = false;
                     inputs.Clear();
 
-                    // if the keybind alrady exists
-                    var uniqueValues = new HashSet<KeyCode>();
-                    var duplicates = new List<string>();
-
-                    foreach (var kvp in currentKeyBinds)
-                    {
-                        if (!uniqueValues.Add(kvp.Value))
-                        {
-                            duplicates.Add(kvp.Key);
-                            break;
-                        }
-                    }
-                    
-                    if (duplicates.Count != 0)
-                    {
-                        for (int i = 0; i < texts.Length; i++) {
-                            if (texts[i].text == duplicates[0])
-                            {
-                                buttons[i].GetComponentInChildren<TextMeshProUGUI>().color = Color.red;
-                                break;
-                            }
-                        }
-                    }
                     // TODO: add code to actually change the input values
                     // i.e if movement key bind are changed it should change in-game
                 }
