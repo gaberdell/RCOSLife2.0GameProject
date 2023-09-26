bd26d3f8690a0941164134a38dc64aa4baeec3ed
Julian Parker-Sartori <132828842+becausej@users.noreply.github.com>
2023-09-26 17:33:24 -0400

    Made my scene

---
+++ Such Life/Assets/Scripts/Item/Axe.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class Axe : Weapon {
+    numEnemiesCanHit = 1;
+    range = 1;
+    attackSpeed = 1.0;
+    armorPenetration = 0.0;
+    //critDamage = 0.0;
+    isOneHanded = true;
+}
