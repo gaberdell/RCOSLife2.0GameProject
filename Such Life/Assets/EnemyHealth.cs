9349454b0b1049256fe84da3c6b913dfb010b7c7
Jason Zheng <zhengj8@rpi.edu>
2023-10-13 17:27:29 -0400

    created combatTesting scene and added ghost animations

---
+++ Such Life/Assets/EnemyHealth.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    //public Animator animator;
+    public int maxHealth = 100;
+    int currentHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        // Play hurt animation
+        if(currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log("Enemy died!");
+        //Die animation
+        //animator.SetBool("IsDead",true);
+        //Disable the enemy
+        this.enabled = false;
+        GetComponent<Collider2D>().enabled = false;
+        GetComponent<EnemyMovement>().enabled = false;
+    }
+    
+}
