fd434f587c4d8a607001115924df50bdd0aee102
Jason Zheng <zhengj8@rpi.edu>
2023-10-13 17:41:31 -0400

    applying animations to enemy

--- Such Life/Assets/EnemyHealth.cs
+++ Such Life/Assets/EnemyHealth.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour
 {
-    //public Animator animator;
+    public Animator animator;
     public int maxHealth = 100;
     int currentHealth;
     // Start is called before the first frame update
@@ -18,6 +18,7 @@ public class EnemyHealth : MonoBehaviour
         currentHealth -= damage;
 
         // Play hurt animation
+        animator.SetTrigger("Hurt");
         if(currentHealth <= 0)
         {
             Die();
@@ -28,7 +29,7 @@ public class EnemyHealth : MonoBehaviour
     {
         Debug.Log("Enemy died!");
         //Die animation
-        //animator.SetBool("IsDead",true);
+        animator.SetBool("IsDead",true);
         //Disable the enemy
         this.enabled = false;
         GetComponent<Collider2D>().enabled = false;
