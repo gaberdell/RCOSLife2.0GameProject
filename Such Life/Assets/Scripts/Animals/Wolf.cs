92ee52adab7e0b9f6b6f82dd2de074c9d5e7453c
SharpBit <31069084+SharpBit@users.noreply.github.com>
2023-10-17 16:45:02 -0400

    wolf prefab and pack manager start

--- Such Life/Assets/Scripts/Animals/Wolf.cs
+++ Such Life/Assets/Scripts/Animals/Wolf.cs
@@ -34,6 +34,12 @@ public class Wolf : AnimalBase
         self = navi.gameObject;
         navi.updateRotation = false;
         navi.updateUpAxis = false;
+
+        // NavMeshHit closestHit;
+        // if (NavMesh.SamplePosition(gameObject.transform.position, out closestHit, 500f, NavMesh.AllAreas))
+        //     gameObject.transform.position = closestHit.position;
+        // else
+        //     Debug.LogError("Could not find position on NavMesh!");
         food = null;
         foodtypes = new List<string>();
         foodtypes.Add("CanFly");
