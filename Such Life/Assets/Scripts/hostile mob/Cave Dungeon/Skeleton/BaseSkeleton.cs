6427a7e84cd6362ee6f5a38730429d46fa180ed8
SpadeZap <72225926+SpadeZap@users.noreply.github.com>
2023-10-31 21:08:27 -0400

    Changed Base Skeleton, by adding movement.

--- Such Life/Assets/Scripts/hostile mob/Cave Dungeon/Skeleton/BaseSkeleton.cs
+++ Such Life/Assets/Scripts/hostile mob/Cave Dungeon/Skeleton/BaseSkeleton.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 
 public class BaseSkeleton : mobBase
 {
-
+    public Animator spanim;
+    [SerializeField]
+    public float prevX;
+    public float prevY;
     public float invincibilityDuration = 3.0f;
     protected bool isInvincible = false;
 
@@ -35,39 +39,162 @@ public class BaseSkeleton : mobBase
     {
         currHealth = maxHealth;
         SetCrumpledState(true); // Initially in crumpled state
+         monsterBody = GetComponent<Rigidbody2D>();
+        monsterBody.drag = 10f;
+        
+        target = GameObject.Find("MC").transform;
+        navi = GetComponent<NavMeshAgent>();
+        navi.acceleration = 200;
+        navi.stoppingDistance = 1f;
+        navi.autoBraking = false;
+        navi.updateRotation = false;
+        navi.updateUpAxis = false;
+
+        currState = State.Wander;
+        currPosition = new Vector2(transform.position.x, transform.position.y);
+        prevX = 0;
+        prevY = 0;
     }
 
     protected virtual void Update()
     {
-        if (playerInRange)
+        prevX = currPosition.x;
+        prevY = currPosition.y;
+        target = GameObject.Find("MC").transform;
+        time += 1f * Time.deltaTime;
+        distance = Vector2.Distance(target.position, currPosition);
+        StateChange();
+
+        //monsterBody.MovePosition(currPosition);
+        navi.isStopped = false;
+        navi.acceleration = 200;
+        if (currState == State.Chasing)
         {
-            // If player is in proximity, get up and become vulnerable
-            SetCrumpledState(false);
-
-            // Check if it's time to perform a bone attack
-            if (Time.time - lastBoneAttackTime >= boneAttackCooldown)
+            spanim.SetBool("Attacking", false);
+            chasing(distance);
+            if (target.position.x - currPosition.x > 2f && Mathf.Abs(currPosition.x - target.position.x) > Mathf.Abs(currPosition.y - target.position.y) * 0.4f)
             {
-                PerformBoneAttack();
+                spanim.SetFloat("Xvel", 1f);
             }
-
-            // Check if it's time to perform a finger attack
-            if (Time.time - lastFingerAttackTime >= fingerAttackCooldown)
+            else if (target.position.x - currPosition.x  < -2f && Mathf.Abs(currPosition.x - target.position.x) > Mathf.Abs(currPosition.y - target.position.y) * 0.4f)
             {
-                PerformFingerAttack();
+                spanim.SetFloat("Xvel", -1f);
+            }
+            else
+            {
+                spanim.SetFloat("Xvel", 0f);
+            }
+            if (target.position.y - currPosition.y > 2f && Mathf.Abs(currPosition.y - target.position.y) > Mathf.Abs(currPosition.x - target.position.x) * 0.4f)
+            {
+                spanim.SetFloat("Yvel", 1f);
+            }
+            else if (target.position.y -currPosition.y < -2f && Mathf.Abs(currPosition.y - target.position.y) > Mathf.Abs(currPosition.x - target.position.x) * 0.4f)
+            {
+                spanim.SetFloat("Yvel", -1f);
+            }
+            else
+            {
+                spanim.SetFloat("Yvel", 0f);
+            }
+        }
+        else if (currState == State.Wander)
+        {   
+            if (time >= time_move){
+                wander();
+                time = 0;
+            }
+            if (currPosition.x - prevX > 0.001f && Mathf.Abs(currPosition.x - prevX) > Mathf.Abs(currPosition.y - prevY) * 0.4f)
+            {
+                spanim.SetFloat("Xvel", 1f);
+            }
+            else if (currPosition.x - prevX < -0.001f && Mathf.Abs(currPosition.x - prevX) > Mathf.Abs(currPosition.y - prevY) * 0.4f)
+            {
+                spanim.SetFloat("Xvel", -1f);
+            }
+            else
+            {
+                spanim.SetFloat("Xvel", 0f);
+            }
+            if (currPosition.y - prevY > 0.001f && Mathf.Abs(currPosition.y - prevY) > Mathf.Abs(currPosition.x - prevX) * 0.4f)
+            {
+                spanim.SetFloat("Yvel", 1f);
+            }
+            else if (currPosition.y - prevY < -0.001f && Mathf.Abs(currPosition.y - prevY) > Mathf.Abs(currPosition.x - prevX) * 0.4f)
+            {
+                spanim.SetFloat("Yvel", -1f);
+            }
+            else
+            {
+                spanim.SetFloat("Yvel", 0f);
             }
         }
 
-        if (isInvincible)
-        {
-            if (Time.time > timeToRecover)
-            {
-                // Skeleton becomes vulnerable again
-                isInvincible = false;
-                skeletonAnimator.SetTrigger("Recover");
-            }
-        }
+        // if (playerInRange)
+        // {
+        //     // If player is in proximity, get up and become vulnerable
+        //     SetCrumpledState(false);
+
+        //     // Check if it's time to perform a bone attack
+        //     if (Time.time - lastBoneAttackTime >= boneAttackCooldown)
+        //     {
+        //         PerformBoneAttack();
+        //     }
+
+        //     // Check if it's time to perform a finger attack
+        //     if (Time.time - lastFingerAttackTime >= fingerAttackCooldown)
+        //     {
+        //         PerformFingerAttack();
+        //     }
+        // }
+
+        // if (isInvincible)
+        // {
+        //     if (Time.time > timeToRecover)
+        //     {
+        //         // Skeleton becomes vulnerable again
+        //         isInvincible = false;
+        //         skeletonAnimator.SetTrigger("Recover");
+        //     }
+        // }
     }
 
+    void wander()
+    {
+        //float oldPosX = currPosition.x;
+        PositionChange();
+        //flipSprite(oldPosX);
+    }
+
+    void chasing(float distance)
+    {
+        float frame_speed = speed * Time.deltaTime;
+        //angle = Mathf.Atan2((target.position.y - currPosition.y) , (target.position.x - currPosition.x));
+        //Vector2 conversion = currPosition;
+        //navi.SetDestination(conversion);
+        currPosition = Vector2.MoveTowards(currPosition,target.position, frame_speed);
+        //print("Debug: speed is" + speed);
+       // print("Debug: speed per frame is" + frame_speed);
+        //flipSprite(oldPosX);
+    }
+
+    public override void PositionChange()
+    { 
+        float posxmin = transform.position.x - speed;
+        float posxmax = transform.position.x + speed;
+        float posymin = transform.position.y - speed;
+        float posymax = transform.position.y + speed;
+        currPosition = new Vector2(Random.Range(posxmin, posxmax), Random.Range(posymin, posymax));
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        navi.isStopped = true;
+        if (collision.gameObject.tag == "Player")
+        {
+            navi.isStopped = false;
+            //bounce();
+        }
+    }
     public void TakeDamage(int damage)
     {
         if (!isInvincible)
@@ -165,6 +292,23 @@ public class BaseSkeleton : mobBase
                 // Set the last attack time
                 lastFingerAttackTime = Time.time;
             }
+        }
+    }
+
+    void StateChange()
+    {
+        //
+        
+        if (distance <= alertRange && !playerSighted)
+        {
+            playerSighted = true;
+            currState = State.Chasing;
+           // flipSprite(-target.position.x);
+        }
+        else if(distance >= alertRange)
+        {
+            playerSighted = false;
+            currState = State.Wander;
         }
     }
 }
