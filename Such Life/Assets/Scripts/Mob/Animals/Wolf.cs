744dfaed1ac18fd8fa4871ea0a6c23eb52e23e68
QuackGameDev <107073102+QuackGameDev@users.noreply.github.com>
2023-11-03 16:49:37 -0400

    Merge branch 'main' into NPCs
    It kinda works

---
+++ Such Life/Assets/Scripts/Mob/Animals/Wolf.cs
@@ -0,0 +1,120 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wolf : AnimalBase
+{
+    private bool dead;
+    private GameObject self;
+    int stored;
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxHealth = 100;
+        currHealth = maxHealth;
+        currMaxSpeed = 1;
+        walkspeed = 1;
+        runspeed = 2;
+        awareness = 25;
+        currState = State.Idling;
+        hungerCap = 100f;
+        hunger = 100f;
+        hungerDrain = 0.05f;
+
+        position = new Vector2(transform.position.x, transform.position.y);
+        newposition = position;
+        time = 0f;
+        timeDelay = 1f;
+        //object initialization
+        player = GameObject.Find("MC");
+        animate = GetComponent<Animator>();
+        Sprite = GetComponent<SpriteRenderer>();
+        navi = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        self = navi.gameObject;
+        // self.Class = this;
+        navi.updateRotation = false;
+        navi.updateUpAxis = false;
+
+        food = null;
+        foodtypes = new List<string>();
+        foodtypes.Add("CanFly");
+        //collider stuff
+        stored = self.layer;
+        dc = GetComponent<BoxCollider2D>();
+        //set inital internal varibles
+        dead = false;
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currHealth <= 0) {
+            currHealth = 0;
+            currState = State.Dying;
+            dead = true;
+            Sprite.flipY = dead;
+        }
+
+        time = time + 1f * Time.deltaTime;
+        if (time >= timeDelay) {
+            time = 0f;
+
+            //If the hunger is greater than or equal to 80, the wolf can heal.
+            if (hunger >= 80)
+            {
+                heal(1);
+            }
+            //If the hunger is less than or equal to 30, it starts looking for food
+            if (hunger <= 30)
+            {
+                navi.speed = runspeed;
+                currState = State.Running;
+                LookForFood(foodtypes);
+            }
+
+            //
+            //If the hunger is 0, it starts dying
+            if (hunger <= 0)
+            {
+                if (hunger < 0)
+                {
+                    hunger = 0;
+                }
+                takeDamage(1);
+            }
+            //Hunger drains if its not 0
+            else
+            {
+                hunger = hunger - hungerCap * hungerDrain;
+                // print(hunger);
+            }
+        }
+        //While the timeDelay isn't met
+        else {
+            if (currState == State.Following)
+            {
+                navi.speed = runspeed;
+                moveTo(player.transform.position);
+            }
+            position = transform.position;
+        }
+    }
+
+    public override void OnCollisionEnter2D(Collision2D collision)
+    {
+        base.OnCollisionEnter2D(collision);
+        OnCollisionEnter2D(collision);
+        // Try to eat non-null food and stop moving
+        if (collision.gameObject == food) {
+            currState = State.Eating;
+            hunger += 50;
+            heal(20);
+            Destroy(food);
+            food = null;
+            moveTo(transform.position);
+            currState = State.Idling;
+        }
+    }
+}
---
+++ Such Life/Assets/Scripts/UI/InventoryUI/InventoryUIController.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
+
+/* Base codes provided by: Dan Pos - Game Dev Tutorials! with modification */
+
+public class InventoryUIController : MonoBehaviour
+{
+
+    [FormerlySerializedAs("chestPanel")] public DynamicInventoryDisplay inventoryPanel;
+    public GameObject dynamicText;
+    public DynamicInventoryDisplay playerBackpackPanel;
+
+    public playerAction playerControl;
+    
+
+    private void Awake()
+    {
+        playerControl = new playerAction();
+        dynamicText.SetActive(false);
+        inventoryPanel.gameObject.SetActive(false);
+        playerBackpackPanel.gameObject.SetActive(false);
+    }
+
+    private void OnEnable()
+    {
+        playerControl.Enable();
+        InventoryHolder.OnDynamicInventoryDisplayRequested += ShowInventory;
+        PlayerInventoryHolder.OnPlayerInventoryDisplayRequested += ShowPlayerInventory;
+        EventManager.closeInventoryUIEvent += CloseInventory;
+    }
+
+    private void OnDisable()
+    {
+        playerControl.Disable();
+        InventoryHolder.OnDynamicInventoryDisplayRequested -= ShowInventory;
+        PlayerInventoryHolder.OnPlayerInventoryDisplayRequested -= ShowPlayerInventory;
+        EventManager.closeInventoryUIEvent -= CloseInventory;
+    }
+
+
+    // Change it so that the player can use mouse cursor to open up the chest if they are directly in front of them using the interact button or right click 
+    // To open player's backpack, they have to press tab and to close it, either press tab or escape button
+    
+    public void CloseInventory(bool closePlayerInventory)
+    {
+        //check to see if you press the key or not
+        
+
+        if (inventoryPanel.gameObject.activeInHierarchy)
+        {
+            dynamicText.SetActive(false);
+            inventoryPanel.gameObject.SetActive(false);
+        }
+
+        if (playerBackpackPanel.gameObject.activeInHierarchy && closePlayerInventory)
+        {
+            playerBackpackPanel.gameObject.SetActive(false);
+        }
+    }
+    
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool closeInventoryKeyPressed = playerControl.Player.CloseInventory.WasPressedThisFrame();
+        if (closeInventoryKeyPressed) {
+            EventManager.CloseInventoryUI(true);
+        }
+           
+    }
+    
+    void ShowInventory(InventorySystem inventoryToShow, int offset)
+    {
+        inventoryPanel.RefreshDynamicInventory(inventoryToShow, offset);
+        inventoryPanel.gameObject.SetActive(true);
+    }
+
+
+    void ShowPlayerInventory(InventorySystem inventoryToShow, int offset)
+    {
+        Debug.Log(inventoryToShow);
+        playerBackpackPanel.RefreshDynamicInventory(inventoryToShow, offset);
+        playerBackpackPanel.gameObject.SetActive(true);
+    }
+
+
+}
