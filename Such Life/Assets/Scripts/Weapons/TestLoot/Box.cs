401e5b20cbeeb6141f353a6732d02edd9f5bfcb5
noobzhuz <75611029+noobzhuz@users.noreply.github.com>
2024-02-11 16:08:01 -0500

    Gameobject for testing loot
    Set up vscode environment. Create a box object to test loot system

---
+++ Such Life/Assets/Scripts/Weapons/TestLoot/Box.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class Box : MonoBehaviour
+{   
+    public GameObject gameObject;
+    void Destroy(GameObject gameObject)
+    {
+        GameObject.Destroy(gameObject);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
