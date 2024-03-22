db00b215a5ccaffed5b876da87105040e33a90c0
noobzhuz <75611029+noobzhuz@users.noreply.github.com>
2024-03-22 17:02:01 -0400

    Refactoring
    Complete refactoring of the weapon base and the loot system

--- Such Life/Assets/Scripts/Weapons/WeaponBag.cs
+++ Such Life/Assets/Scripts/Weapons/WeaponBag.cs
@@ -8,30 +8,34 @@ public class WeaponBag : MonoBehaviour
     public List<Weapon> WeaponList = new List<Weapon>();
     Weapon GetDroppedWeapon()
     {
-        List<Weapon> PossibleWeapons = new List<Weapon>();
-        foreach(Weapon Item in WeaponList)
-        {
-            Weapon newItem = ScriptableObject.Instantiate(Item);
+        var possibleWeapons = new List<Weapon>();
+        foreach(Weapon item in WeaponList){
+            Weapon newItem = ScriptableObject.Instantiate(item);
             newItem.constructor();
-            PossibleWeapons.Add(newItem);
+            possibleWeapons.Add(newItem);
         }
-        if(PossibleWeapons.Count > 0)
-        {
-            Weapon DropWeapon = PossibleWeapons[Random.Range(0,PossibleWeapons.Count)];
-            return DropWeapon;
+
+        if(possibleWeapons.Count > 0){
+
+            Weapon dropWeapon = possibleWeapons[Random.Range(0,possibleWeapons.Count)];
+            return dropWeapon;
         }
+
         Debug.Log("no weapon dropped");
         return null;
     }
 
     public void InstantiateWeapon(Vector3 Position)
     {
-        Weapon DroppedItem = GetDroppedWeapon();
-        if(DroppedItem  != null)
-        {
-            GameObject LootGameObject = Instantiate(WeaponPrefab, Position, Quaternion.identity);
-            LootGameObject.GetComponent<SpriteRenderer>().sprite = DroppedItem.sprite; //now we need a sprite for weapon drop
-            LootGameObject.GetComponent<WeaponData>().AssignSO(DroppedItem);
+        Weapon droppedItem = GetDroppedWeapon();
+
+        if(droppedItem  != null){
+
+            GameObject lootGameObject = Instantiate(WeaponPrefab, Position, Quaternion.identity);
+            lootGameObject.GetComponent<SpriteRenderer>().sprite = droppedItem.sprite; //now we need a sprite for weapon drop
+            lootGameObject.GetComponent<WeaponData>().AssignSO(droppedItem);
         }
+
     }
+    
 }
--- Such Life/Assets/Scripts/Weapons/WeaponData.cs
+++ Such Life/Assets/Scripts/Weapons/WeaponData.cs
@@ -5,12 +5,10 @@ using System.Linq.Expressions;
 using JetBrains.Annotations;
 using UnityEngine;
 
-public class WeaponData : MonoBehaviour
-{   
-    private Weapon WeaponSO; // scriptableobject
-    public void AssignSO(Weapon weaponSO)
-    {
-        WeaponSO = weaponSO;
+public class WeaponData : MonoBehaviour{   
+    private Weapon _weaponSO; // scriptableobject
+    public void AssignSO(Weapon weaponSO){
+        _weaponSO = weaponSO;
     }
 }
 
