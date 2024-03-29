6ea65e302734c7b56b45ee1f3d4a9eb20f7383ad
Jason Zheng <zhengj8@rpi.edu>
2024-03-29 17:17:10 -0400

    very basic player-enemy attack and enemy death

--- Such Life/Assets/Scripts/Mob/HostileMob/EnemyMovement.cs
+++ Such Life/Assets/Scripts/Mob/HostileMob/EnemyMovement.cs
@@ -7,22 +7,28 @@ public class EnemyMovement : MonoBehaviour
     //Private Variables
     [SerializeField] private SpriteRenderer sprite;
     [SerializeField] private float speed = 0.5f;
-    [SerializeField] private Transform Player;
+    [SerializeField] private GameObject Player;
     [SerializeField] private float damageAmount = 20f;
+
+    private void Awake()
+    {
+        sprite = Player.GetComponent<SpriteRenderer>();
+    }
 
     // Update is called once per frame
     private void Update () {
-        Vector3 displacement = Player.position - transform.position;
+        Vector3 displacement = Player.transform.position - transform.position;
         displacement = displacement.normalized;
-        if (Vector2.Distance (Player.position, transform.position) > 1.0f) {
+        if (Vector2.Distance (Player.transform.position, transform.position) > 1.0f) {
             sprite.color = Color.red;
             sprite.color = Color.white;
             transform.position += (displacement * speed * Time.deltaTime);
                         
         }
         else {
-            Player.position += (displacement);
+            Player.transform.position += (displacement);
             sprite.color = Color.red;
+
             EventManager.DealDamage(Player.gameObject, damageAmount);
         }
     }
