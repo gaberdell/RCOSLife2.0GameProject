9e540a6045b27f02080a78959e398936686a405a
gaberdell <spegitthicode@gmail.com>
2024-11-01 23:52:13 -0400

    Made basic block destroying thing
    Made basic block destroying thing

--- Such Life/Assets/Scripts/Player/Inventory/BlockDestroyer.cs
+++ Such Life/Assets/Scripts/Player/Inventory/BlockDestroyer.cs
@@ -8,6 +8,14 @@ public class BlockDestroyer : BlockInteraction
     [SerializeField]
     BlockItemDictionary blockItemDictionary;
 
+    [SerializeField]
+    GameObject blockPreview;
+
+    [SerializeField]
+    Vector3 destroyPlaceOffset = new Vector3(0.5f, 0.5f, 0.5f);
+
+    Vector3 destroyObjectPos = Vector3.zero;
+
     // Start is called before the first frame update
     override protected void Start()
     {
@@ -17,44 +25,55 @@ public class BlockDestroyer : BlockInteraction
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        Vector3 mousePos = Input.mousePosition;
+
+        Vector3 realWorldSpace = Camera.main.ScreenToWorldPoint(mousePos);
+
+        realWorldSpace.z = transform.position.z;
+
+        //Directional Vector
+        Vector3 distanceFromPlayerVector = (realWorldSpace - transform.position);
+
+        float lengthOfDistanceVector = distanceFromPlayerVector.magnitude;
+
+
+        if (lengthOfDistanceVector > playerReach)
+        {
+            distanceFromPlayerVector = (distanceFromPlayerVector.normalized * playerReach);
+        }
+
+        Vector3 tileToDestroy = isTilePartOfRay(placingTileMap, transform.position, distanceFromPlayerVector, testIfBlockStepSize, out realWorldSpace);
+
+        Vector3Int destroyPos = placingTileMap.WorldToCell(tileToDestroy);
+
+        TileBase checkedTile;
+
+        destroyObjectPos = destroyPos + destroyPlaceOffset;
+        if ((checkedTile = placingTileMap.GetTile(destroyPos)) != null)
         {
 
-            Vector3 mousePos = Input.mousePosition;
+        }
+        else
+        {
+            blockPreview.SetActive(false);
+        }
+        blockPreview.transform.position = destroyObjectPos;
 
-            Vector3 realWorldSpace = Camera.main.ScreenToWorldPoint(mousePos);
-
-            realWorldSpace.z = transform.position.z;
-
-            //Directional Vector
-            Vector3 distanceFromPlayerVector = (realWorldSpace - transform.position);
-
-            float lengthOfDistanceVector = distanceFromPlayerVector.magnitude;
-
-
-            if (lengthOfDistanceVector > playerReach)
-            {
-                distanceFromPlayerVector = (distanceFromPlayerVector.normalized * playerReach);
-            }
-
-
-            Vector3 tileToDestroy = isTilePartOfRay(placingTileMap, transform.position, distanceFromPlayerVector, testIfBlockStepSize, out realWorldSpace);
-
-            Vector3Int destroyPos = placingTileMap.WorldToCell(tileToDestroy);
-
-            TileBase checkedTile;
-
+        //If right click down then check if the destroyable tile would place an object
+        // if so desetroy it
+        if (Input.GetKeyDown(KeyCode.Mouse0)) 
+        {
             if ((checkedTile = placingTileMap.GetTile(destroyPos)) != null)
             {
-
                 Debug.Log(checkedTile.name);
                 placingTileMap.SetTile(destroyPos, null);
                 if (blockItemDictionary.tileToItemPrefabDict[checkedTile])
                 {
                     GameObject newBlockItem = Instantiate(blockItemDictionary.tileToItemPrefabDict[checkedTile]);
-                    newBlockItem.transform.position = destroyPos;
+                    newBlockItem.transform.position = destroyObjectPos;
+                    
                 }
-                //Find Some way to tell what block was chosen and destroy it
+                
             }
         }
     }
