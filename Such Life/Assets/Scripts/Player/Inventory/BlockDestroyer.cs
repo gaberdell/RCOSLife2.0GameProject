d888c2260919c4c4eff4f6af33542ecc78d7c809
gaberdell <spegitthicode@gmail.com>
2024-11-07 09:31:09 -0500

    Working on refining Block destroyer
    Refining block destroyer also trying at a branchless programming thingy

--- Such Life/Assets/Scripts/Player/Inventory/BlockDestroyer.cs
+++ Such Life/Assets/Scripts/Player/Inventory/BlockDestroyer.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
+//https://discussions.unity.com/t/how-do-i-make-an-object-look-at-an-another-object-in-top-down-2d-c/104849
+//For the rotation info thanks!
+
+//Info on branchlessly doing stuff in https://dev.to/jobinrjohnson/branchless-programming-does-it-really-matter-20j4
+
+
 public class BlockDestroyer : BlockInteraction
 {
     [SerializeField]
@@ -15,6 +21,9 @@ public class BlockDestroyer : BlockInteraction
     Vector3 destroyPlaceOffset = new Vector3(0.5f, 0.5f, 0.5f);
 
     Vector3 destroyObjectPos = Vector3.zero;
+
+    float previewAngleOffset = -90;
+    float roundPreviewAnglesTo = 90;
 
     // Start is called before the first frame update
     override protected void Start()
@@ -36,11 +45,10 @@ public class BlockDestroyer : BlockInteraction
 
         float lengthOfDistanceVector = distanceFromPlayerVector.magnitude;
 
-
-        if (lengthOfDistanceVector > playerReach)
-        {
-            distanceFromPlayerVector = (distanceFromPlayerVector.normalized * playerReach);
-        }
+        
+        //Branchless way of trying to find the max
+        distanceFromPlayerVector =    System.Convert.ToInt32(lengthOfDistanceVector <= playerReach) * distanceFromPlayerVector 
+                                    + System.Convert.ToInt32(lengthOfDistanceVector > playerReach) * (distanceFromPlayerVector.normalized * playerReach);
 
         Vector3 tileToDestroy = isTilePartOfRay(placingTileMap, transform.position, distanceFromPlayerVector, testIfBlockStepSize, out realWorldSpace);
 
@@ -51,19 +59,23 @@ public class BlockDestroyer : BlockInteraction
         destroyObjectPos = destroyPos + destroyPlaceOffset;
         if ((checkedTile = placingTileMap.GetTile(destroyPos)) != null)
         {
-
+            blockPreview.SetActive(true);
+            blockPreview.transform.position = destroyObjectPos;
+            //Change this slow ass math later dawg we gotta do atan for this
+            float angle = Mathf.Round(((Mathf.Atan2(distanceFromPlayerVector.y, distanceFromPlayerVector.x) * Mathf.Rad2Deg) + previewAngleOffset) / roundPreviewAnglesTo) * roundPreviewAnglesTo;
+            blockPreview.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
         }
         else
         {
             blockPreview.SetActive(false);
         }
-        blockPreview.transform.position = destroyObjectPos;
+        
 
         //If right click down then check if the destroyable tile would place an object
         // if so desetroy it
         if (Input.GetKeyDown(KeyCode.Mouse0)) 
         {
-            if ((checkedTile = placingTileMap.GetTile(destroyPos)) != null)
+            if (checkedTile != null)
             {
                 Debug.Log(checkedTile.name);
                 placingTileMap.SetTile(destroyPos, null);
