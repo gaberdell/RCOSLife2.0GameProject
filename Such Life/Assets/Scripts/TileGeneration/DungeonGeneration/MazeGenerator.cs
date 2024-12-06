6dfb580e9bc585a41d786271df4e8f16232979ff
gaberdell <spegitthicode@gmail.com>
2024-12-06 15:50:41 -0500

    Dungeon Generator Stuff Its pretty neat
    Dungeon Generator basically it has some stuff and then has some like split and turning random chances!

--- Such Life/Assets/Scripts/TileGeneration/DungeonGeneration/MazeGenerator.cs
+++ Such Life/Assets/Scripts/TileGeneration/DungeonGeneration/MazeGenerator.cs
@@ -5,6 +5,14 @@ using UnityEngine.Tilemaps;
 
 public class MazeGenerator : MonoBehaviour
 {
+    [Range(0f,1f)]
+    [SerializeField]
+    float directionChangeChance;
+
+    [Range(0f, 1f)]
+    [SerializeField]
+    float splitChangeChance;
+
     [SerializeField]
     Vector2Int sizeOfMaze;
 
@@ -29,12 +37,12 @@ public class MazeGenerator : MonoBehaviour
             }
         }
 
-        GenerateCell(0, 0, sizeOfMaze.x, sizeOfMaze.y);
+        GenerateCell(9, 9, 8, 9, sizeOfMaze.x, sizeOfMaze.y);
 
         GenerateCellToTileMap();
     }
 
-    void GenerateCell(int currentX, int currentY, int maxX, int maxY)
+    void GenerateCell(int currentX, int currentY, int prevX, int prevY, int maxX, int maxY)
     {
         if (basicCellMazeArray[currentX, currentY].cellData.HasFlag(OpenCells.Visited))
         {
@@ -44,64 +52,118 @@ public class MazeGenerator : MonoBehaviour
             basicCellMazeArray[currentX, currentY].cellData |= OpenCells.Visited;
         }
 
-        if (currentX + 1 < maxX)
+        //Sanity Check Moves
+        if (currentX + 1 == maxX)
         {
-            basicCellMazeArray[currentX, currentY].cellData |= OpenCells.EastOpen;
-            basicCellMazeArray[currentX+1, currentY].cellData |= OpenCells.WestOpen;
-            GenerateCell(currentX + 1, currentY, maxX, maxY);
+            basicCellMazeArray[currentX-1, currentY].cellData |= OpenCells.EastOpen;
+            basicCellMazeArray[currentX, currentY].cellData |= OpenCells.WestOpen;
+            GenerateCell(currentX, currentY, currentX, currentY, maxX, maxY);
         }
-
-        if (currentY + 1 < maxY)
+        else if (currentY + 1 == maxY)
         {
-            basicCellMazeArray[currentX, currentY].cellData |= OpenCells.NorthOpen;
-            basicCellMazeArray[currentX, currentY + 1].cellData |= OpenCells.SouthOpen;
-            GenerateCell(currentX, currentY + 1, maxX, maxY);
+            basicCellMazeArray[currentX, currentY-1].cellData |= OpenCells.NorthOpen;
+            basicCellMazeArray[currentX, currentY].cellData |= OpenCells.SouthOpen;
+            GenerateCell(currentX, currentY, currentX, currentY, maxX, maxY);
         }
-
-        if (currentX - 1 >= 0)
+        else if (currentX - 1 == 0)
         {
             basicCellMazeArray[currentX, currentY].cellData |= OpenCells.WestOpen;
             basicCellMazeArray[currentX - 1, currentY].cellData |= OpenCells.EastOpen;
-            GenerateCell(currentX - 1, currentY, maxX, maxY);
+            GenerateCell(currentX - 1, currentY, currentX, currentY, maxX, maxY);
         }
-
-        if (currentY - 1 >= 0)
+        else if (currentY - 1 == 0)
         {
             basicCellMazeArray[currentX, currentY].cellData |= OpenCells.SouthOpen;
             basicCellMazeArray[currentX, currentY - 1].cellData |= OpenCells.NorthOpen;
-            GenerateCell(currentX, currentY - 1, maxX, maxY);
+            GenerateCell(currentX, currentY - 1, currentX, currentY, maxX, maxY);
+        }
+        else
+        {
+            
+            if (currentX - prevX > 0)
+            {
+                basicCellMazeArray[currentX - 1, currentY].cellData |= OpenCells.EastOpen;
+                basicCellMazeArray[currentX, currentY].cellData |= OpenCells.WestOpen;
+            }
+            if (currentX - prevX < 0)
+            {
+                basicCellMazeArray[currentX, currentY].cellData |= OpenCells.WestOpen;
+                basicCellMazeArray[currentX - 1, currentY].cellData |= OpenCells.EastOpen;
+            }
+
+            if (currentY - prevY > 0)
+            {
+                basicCellMazeArray[currentX, currentY - 1].cellData |= OpenCells.NorthOpen;
+                basicCellMazeArray[currentX, currentY].cellData |= OpenCells.SouthOpen;
+            }
+            if (currentY - prevY < 0)
+            {
+                basicCellMazeArray[currentX, currentY].cellData |= OpenCells.SouthOpen;
+                basicCellMazeArray[currentX, currentY - 1].cellData |= OpenCells.NorthOpen;
+            }
+
+            //Mutation Chance where we the dungeon crawler has a random chance to change direction
+            float randomVal = Random.Range(0f,1f);
+
+            if (randomVal <= directionChangeChance) {
+                GenerateCell(currentX + currentY - prevY , currentY + currentX - prevX, currentX, currentY, maxX, maxY);
+                if (randomVal <= splitChangeChance)
+                {
+                    GenerateCell(currentX + currentX - prevX, currentY + currentY - prevY, currentX, currentY, maxX, maxY);
+                }
+            }
+            else
+            {
+                GenerateCell(currentX + currentX - prevX, currentY + currentY - prevY, currentX, currentY, maxX, maxY);
+            }
+                
+
+            //GenerateCell(currentX + currentX - prevX, currentY + currentY - prevY, currentX, currentY, maxX, maxY);
         }
     }
 
     void ifCheck(int i, int j)
     {
-        //Flippin annoying but I see no other way
-        if (!basicCellMazeArray[i, j].cellData.HasFlag(OpenCells.NorthOpen))
+        if (basicCellMazeArray[i, j].cellData.HasFlag(OpenCells.Visited))
         {
             tilesToPlaceOn.SetTile(new Vector3Int(3 * i, -3 * j - 2, 0), tileToPlace);
-            tilesToPlaceOn.SetTile(new Vector3Int(3 * i + 1, -3 * j - 2, 0), tileToPlace);
             tilesToPlaceOn.SetTile(new Vector3Int(3 * i + 2, -3 * j - 2, 0), tileToPlace);
+
+            tilesToPlaceOn.SetTile(new Vector3Int(3 * i, -3 * j, 0), tileToPlace);
+            tilesToPlaceOn.SetTile(new Vector3Int(3 * i + 2, -3 * j, 0), tileToPlace);
+
+            tilesToPlaceOn.SetTile(new Vector3Int(3 * i + 2, -3 * j - 2, 0), tileToPlace);
+            tilesToPlaceOn.SetTile(new Vector3Int(3 * i + 2, -3 * j, 0), tileToPlace);
+
+            tilesToPlaceOn.SetTile(new Vector3Int(3 * i, -3 * j - 2, 0), tileToPlace);
+            tilesToPlaceOn.SetTile(new Vector3Int(3 * i, -3 * j, 0), tileToPlace);
+        }
+
+        //Flippin annoying but I see no other way
+        if (!basicCellMazeArray[i, j].cellData.HasFlag(OpenCells.NorthOpen))
+        { 
+            tilesToPlaceOn.SetTile(new Vector3Int(3 * i + 1, -3 * j - 2, 0), tileToPlace);
         }
 
         if (!basicCellMazeArray[i, j].cellData.HasFlag(OpenCells.SouthOpen))
         {
-            tilesToPlaceOn.SetTile(new Vector3Int(3 * i, -3 * j, 0), tileToPlace);
+            
             tilesToPlaceOn.SetTile(new Vector3Int(3 * i + 1, -3 * j, 0), tileToPlace);
-            tilesToPlaceOn.SetTile(new Vector3Int(3 * i + 2, -3 * j, 0), tileToPlace);
+            
         }
 
         if (!basicCellMazeArray[i, j].cellData.HasFlag(OpenCells.EastOpen))
         {
-            tilesToPlaceOn.SetTile(new Vector3Int(3 * i + 2, -3 * j - 2, 0), tileToPlace);
+            
             tilesToPlaceOn.SetTile(new Vector3Int(3 * i + 2, -3 * j - 1, 0), tileToPlace);
-            tilesToPlaceOn.SetTile(new Vector3Int(3 * i + 2, -3 * j, 0), tileToPlace);
+            
         }
 
         if (!basicCellMazeArray[i, j].cellData.HasFlag(OpenCells.WestOpen))
         {
-            tilesToPlaceOn.SetTile(new Vector3Int(3 * i, -3 * j - 2, 0), tileToPlace);
+            
             tilesToPlaceOn.SetTile(new Vector3Int(3 * i, -3 * j - 1, 0), tileToPlace);
-            tilesToPlaceOn.SetTile(new Vector3Int(3 * i, -3 * j, 0), tileToPlace);
+            
         }
     }
 
